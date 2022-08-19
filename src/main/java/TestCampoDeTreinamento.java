93caf636d21d2ed49284337a13424d6e7c632ce5
jagapatr <jagapatr@motorola.com>
2022-08-19 11:32:14 -0300

    Test Campo de treinamento - Cadastro Ok

--- src/main/java/TestCampoDeTreinamento.java
+++ src/main/java/TestCampoDeTreinamento.java
@@ -1,6 +1,9 @@
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class TestCampoDeTreinamento {
 	
@@ -11,9 +14,20 @@ public class TestCampoDeTreinamento {
 		WebDriver driver = new FirefoxDriver();
 		driver.get("file:///" + System.getProperty("user.dir") + "/src/test/resources/componentes.html");
 		
+		driver.findElement(By.id("elementosForm:nome")).sendKeys("Adriano");
+		driver.findElement(By.id("elementosForm:sobrenome")).sendKeys("Patricio");
+		driver.findElement(By.id("elementosForm:sexo:0")).click();
+		driver.findElement(By.id("elementosForm:comidaFavorita:2")).click();
+		new Select(driver.findElement(By.id("elementosForm:escolaridade"))).selectByVisibleText("Superior");
+		new Select(driver.findElement(By.id("elementosForm:esportes"))).selectByVisibleText("Corrida");
+		driver.findElement(By.id("elementosForm:cadastrar")).click();
 		
-		
+		Assert.assertTrue(driver.findElement(By.id("resultado")).getText().startsWith("Cadastrado!"));
+		Assert.assertEquals("Nome: Adriano",driver.findElement(By.id("descNome")).getText());
+		Assert.assertEquals("Sobrenome: Patricio",driver.findElement(By.id("descSobrenome")).getText());
+		Assert.assertEquals("Sexo: Masculino",driver.findElement(By.id("descSexo")).getText());
+		Assert.assertEquals("Comida: Pizza",driver.findElement(By.id("descComida")).getText());	
+		Assert.assertEquals("Escolaridade: superior",driver.findElement(By.id("descEscolaridade")).getText());
+		Assert.assertEquals("Esportes: Corrida",driver.findElement(By.id("descEsportes")).getText());
 	}
-	
-
 }
--- src/main/java/TestGoogle.java
+++ src/main/java/TestGoogle.java
@@ -1,4 +1,3 @@
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,11 +10,12 @@ public class TestGoogle {
 	public void testGoogle() {
 		
 		System.setProperty("webdriver.gecko.driver","C:/geckodriver-v0.31.0-win64/geckodriver.exe"); 
+		
 		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.google.com");
 		
 		Assert.assertEquals("Google",driver.getTitle());
-		driver.quit();
+		//driver.quit();
 		
 	}
 
